144d27de01d08092760736dcafca1a358a71a65a
Laplace-py <pietro.henric@gmail.com>
2023-01-23 22:27:16 -0300

    Universal Data splitting notebook

--- Github_Artigo/utils/split.py
+++ Github_Artigo/utils/split.py
@@ -1,49 +1,77 @@
 import pandas as pd
-from chemprop.data import utils
+from chemprop.data import utils as chem_utils
 from random import randint
 import pandas as pd
 import numpy as np
 import warnings; warnings.simplefilter('ignore')
-seed_val = 42
+from utils.utils import GLOBALS 
+class DataSplitter():
+    def __init__(self) -> None:
+        pass
+    
+    Scaffold_split = GLOBALS.SCAFFOLD_SPLIT
+    Random_split = GLOBALS.RANDOM_SPLIT
+    
+    split_type: int = Random_split or Scaffold_split
+    assert split_type == Random_split or split_type == Scaffold_split, "Invalid split type"
 
-def get_partition_as_df(partition):
-    dict = {'ID': partition.compound_names(), 'SMILES': partition.smiles()}
-    df = pd.DataFrame(dict)
-    df2 = pd.DataFrame(df)
-    #df2[target_names] = pd.DataFrame(df2.targets.values.tolist(), index= df2.index)
-    #df2.drop('targets', axis=1, inplace=True)
-    return df2
+    def get_partition_as_df(self,partition:chem_utils.split_data):
+        partition_dict = {'ID': partition.compound_names(), 'SMILES': partition.smiles()}
+        df = pd.DataFrame(partition_dict)
+        return df
+
+    def random_split(self,dataset:str, sizes:tuple[float,float,float]=(.8,.1,.1) ,seed_val:int=42) -> tuple[pd.DataFrame,pd.DataFrame,pd.DataFrame]:
+        # get target names (assuming that 1st column contains molecule names and 2nd column contains smiles and rest of the columns are targets)
+        #df = pd.read_csv(dataset, sep=",", index_col=None, dtype={'ID': str})
+        #cols = list(df.columns)
+        #target_names = cols[2:]
+
+        assert sum(sizes) == 1, "Sum of sizes should be 1"
+        mol_dataset = chem_utils.get_data(dataset, use_compound_names=True)
+        train, valid, test = chem_utils.split_data(mol_dataset, sizes=sizes, seed=seed_val)
+        train_df = self.get_partition_as_df(train)
+        train_df = train_df[['ID']]
+        valid_df = self.get_partition_as_df(valid)
+        valid_df = valid_df[['ID']]
+        test_df = self.get_partition_as_df(test)
+        test_df = test_df[['ID']]
+        return train_df, valid_df, test_df
+
+    def scaffold_split(self,dataset:str, sizes:tuple[float,float,float]=(.8,.1,.1), seed:int=42) -> tuple[pd.DataFrame,pd.DataFrame,pd.DataFrame]:
+        # get target names (assuming that 1st column contains molecule names and 2nd column contains smiles and rest of the columns are targets)
+        # df = pd.read_csv(dataset, sep=",", index_col=None, dtype={'ID': str})
+        # cols = list(df.columns)
+        # target_names = cols[2:]
+
+        assert sum(sizes) == 1, "Sum of sizes should be 1"
+        mol_dataset = chem_utils.get_data(dataset, use_compound_names=True)
+        train, valid, test = chem_utils.split_data(mol_dataset, split_type="scaffold_balanced", sizes=sizes, seed=seed)
+        train_df = self.get_partition_as_df(train)
+        train_df = train_df[['ID']]
+        valid_df = self.get_partition_as_df(valid)
+        valid_df = valid_df[['ID']]
+        test_df = self.get_partition_as_df(test)
+        test_df = test_df[['ID']]
+        return train_df, valid_df, test_df
+
+    def split_data(self,dataset:str,n_folds:int=5 ,sizes:tuple[float,float,float]=(.8,.1,.1), seed:int=42, split_type=split_type) -> list[tuple[pd.DataFrame,pd.DataFrame,pd.DataFrame]]:
+        """
+        Generates splits for cross validation one at a time to save memory
+        """
+        for i in range(n_folds):
+            if split_type == self.Random_split:
+                yield self.random_split(dataset, sizes, seed),i
+            elif split_type == self.Scaffold_split:
+                yield self.scaffold_split(dataset, sizes, seed),i
+    
+    def merge_data(self,dataset:str, train_df:pd.DataFrame, valid_df:pd.DataFrame, test_df:pd.DataFrame) -> tuple[pd.DataFrame,pd.DataFrame,pd.DataFrame]:
+        
+        df = pd.read_csv(dataset, sep=",", index_col=None, dtype={'ID': str})
+        
+        train_df = pd.merge(df, train_df, on='ID')
+        valid_df = pd.merge(df, valid_df, on='ID')
+        test_df = pd.merge(df, test_df, on='ID')
+        
+        return train_df, valid_df, test_df
 
 
-def random_split(dataset, seed_val):
-    # get target names (assuming that 1st column contains molecule names and 2nd column contains smiles and rest of the columns are targets)
-    df = pd.read_csv(dataset, sep=",", index_col=None, dtype={'ID': str})
-    cols = list(df.columns)
-    #target_names = cols[2:]
-
-    mol_dataset = utils.get_data(dataset, use_compound_names=True)
-    train, valid, test = utils.split_data(mol_dataset, sizes=(0.8, 0.1, 0.1), seed=seed_val)
-    train_df = get_partition_as_df(train)
-    train_df = train_df[['ID']]
-    valid_df = get_partition_as_df(valid)
-    valid_df = valid_df[['ID']]
-    test_df = get_partition_as_df(test)
-    test_df = test_df[['ID']]
-    return train_df, valid_df, test_df
-
-
-def scaffold_split(dataset, seed):
-    # get target names (assuming that 1st column contains molecule names and 2nd column contains smiles and rest of the columns are targets)
-    df = pd.read_csv(dataset, sep=",", index_col=None, dtype={'ID': str})
-    cols = list(df.columns)
-    target_names = cols[2:]
-
-    mol_dataset = utils.get_data(dataset, use_compound_names=True)
-    train, valid, test = utils.split_data(mol_dataset, split_type="scaffold_balanced", sizes=(0.8, 0.1, 0.1), seed=seed_val)
-    train_df = get_partition_as_df(train)
-    train_df = train_df[['ID']]
-    valid_df = get_partition_as_df(valid)
-    valid_df = valid_df[['ID']]
-    test_df = get_partition_as_df(test)
-    test_df = test_df[['ID']]
-    return train_df, valid_df, test_df
--- Github_Artigo/utils/utils.py
+++ Github_Artigo/utils/utils.py
@@ -7,7 +7,7 @@ from rdkit.Chem import DataStructs
 from rdkit.Chem import rdMolDescriptors
 from rdkit.Chem.AllChem import GetMorganFingerprintAsBitVect
 import itertools
-from rdkit import Chem
+
 from rdkit.Chem import rdDepictor
 rdDepictor.SetPreferCoordGen(True)
 from rdkit.Chem.Draw import rdMolDraw2D
@@ -20,6 +20,30 @@ import sklearn.metrics as SK
 from tensorflow.keras import backend as K
 from tensorflow.keras.losses import *
 import seaborn as sns
+from typing import List, Set, Tuple, Union,Callable, Optional, Dict, Any
+from dataclasses import dataclass
+import PIL
+@dataclass
+class ChemType():
+    """
+    Typing only, no implementation.
+    """
+    def __init__(self):
+        pass
+@dataclass
+class PillType():
+    """
+    Typing only, no implementation.
+    """
+    def __init__(self):
+        pass
+@dataclass
+class GLOBALS():
+    
+    CLASSIFICATION: int = 0
+    REGRESSION: int = 1
+    SCAFFOLD_SPLIT: int = 0
+    RANDOM_SPLIT: int = 1
 class Commons():
     def __init__(self):
         pass
@@ -65,7 +89,7 @@ class Shap_Helper():
     def __init__(self):
         pass
     
-    def get_bit_info(self, smiles:list,fp_size:int, radius:int, feat:bool)-> tuple:
+    def get_bit_info(self, smiles:list,fp_size:int, radius:int, feat:bool)-> tuple[list[int],list[int],list[ChemType],list[int]]:
         """
         calcs morgan fingerprints as a numpy array.
         """
@@ -94,16 +118,16 @@ class Shap_Helper():
             range_bit.append(range_int)
         return atom_value, radius_value, mols, range_bit
     
-    def getSmilesFragFromBit (self,mol:Chem, atom:int, raio:int) -> str:
+    def getSmilesFragFromBit (self,mol:Chem, atom:int, radius:int) -> str:
 
-        env = Chem.FindAtomEnvironmentOfRadiusN(mol, atom, raio)
+        env = Chem.FindAtomEnvironmentOfRadiusN(mol, atom, radius)
         amap={}
         submol=Chem.PathToSubmol(mol, env,atomMap=amap)#bit info ecfp in Mol
         fragment = Chem.MolToSmiles(submol)#
 
         return fragment
     
-    def generateFragList(self,mols:list,radius_list:list,atom_list:list,bit_list:list) -> list:
+    def generateFragList(self,mols:list,radius_list:list,atom_list:list,bit_list:list) -> list[dict]:
         fragment_moleculs  = []
         combined_list = []
         
@@ -121,13 +145,13 @@ class Shap_Helper():
         df = pd.DataFrame(fingerprint_array, columns=col_names)
         return df
 
-    def get_top_shap_values(self,shap_dataset:pd.DataFrame,shap_values, top_n = 10):
+    def get_top_shap_values(self,shap_dataset:pd.DataFrame,shap_values, top_n = 10) -> tuple[list[float],list[str]]:
         top_indices = np.argsort(np.abs(shap_values))[-top_n:]
         top_shap_values = shap_values[top_indices]
         top_feature_names = shap_dataset.columns[top_indices]
         return top_shap_values, top_feature_names
 
-    def get_bits_fromBestShaps(self,shap_dataset:pd.DataFrame,shap_values, top_n = 10):
+    def get_bits_fromBestShaps(self,shap_dataset:pd.DataFrame,shap_values, top_n = 10) -> list[list[int]]:
         best_values,best_bits = [],[]
         for i in range(shap_dataset.shape[0]-1):
             values,bits = self.get_top_shap_values(shap_dataset,shap_values[i][0], top_n)
@@ -136,7 +160,7 @@ class Shap_Helper():
             best_bits.append(bits)
         return best_bits
 
-    def draw_highlightedMols(self,mols,frag_list:list,mol_idx:list or int,b_bits:list):
+    def get_highlightedMols(self,mols:ChemType,frag_list:list,mol_idx:list or int,b_bits:list) -> list[PillType]:
 
         highlights = {l:{k:[] for k in range(2048)} for l in range(len(frag_list)) }
         deletes = []
@@ -170,18 +194,20 @@ class TS_Helper():
     def __init__(self):
         """Define model type with Ts_Helper.model_type = Ts_Helper.classification or Ts_Helper.regression"""
         pass
+    
+    Classification = GLOBALS.CLASSIFICATION
+    Regression = GLOBALS.REGRESSION
 
-    Classification = 0
-    Regression = 1
-    model_type = 0
-
+    model_type: int = Classification or Regression
+    assert model_type in [Classification, Regression], "model_type must be either Ts_Helper.Classification or Ts_Helper.Regression"
+    
     MISSING_LABEL_FLAG = -1
     BinaryCrossentropy = T.keras.losses.BinaryCrossentropy()
     CategoricalCrossentropy = T.keras.losses.CategoricalCrossentropy()
     MeanSquaredError = T.keras.losses.MeanSquaredError()
     SparseCategoricalCrossentropy = T.keras.losses.SparseCategoricalCrossentropy() 
 
-    def classification_loss(self,loss_function ,mask_value = MISSING_LABEL_FLAG): #used in multitask classification models (customizable)
+    def classification_loss(self,loss_function:Callable,mask_value:int = MISSING_LABEL_FLAG)-> Callable: #used in multitask classification models (customizable)
     
         """Builds a loss function that masks based on targets
         Args:
@@ -191,7 +217,7 @@ class TS_Helper():
             function: a loss function that acts like loss_function with masked inputs
         """
 
-        def masked_loss_function(y_true, y_pred):  
+        def masked_loss_function(y_true:float, y_pred:float):  
             y_true = float(y_true)
             y_pred = float(y_pred)
             y_true = T.where(T.math.is_nan(y_true), T.zeros_like(y_true), y_true)
@@ -201,7 +227,7 @@ class TS_Helper():
 
         return masked_loss_function
 
-    def regression_loss(self,y_true, y_pred):  #Used in multitask regression models (MSE metric)
+    def regression_loss(self,y_true:float, y_pred:float) -> float:  #Used in multitask regression models (MSE metric)
         y_true = T.where(T.math.is_nan(y_true), T.zeros_like(y_true), y_true)
         loss = T.reduce_mean(
         T.where(T.equal(y_true, 0.0), y_true,
@@ -209,7 +235,7 @@ class TS_Helper():
         
         return loss     
     # adaptative learning rate using during training
-    def get_lr_metric(self,optimizer):
+    def get_lr_metric(self,optimizer:T.optimizers.Optimizer):
         def lr(y_true, y_pred):
             return optimizer.lr
             
@@ -234,7 +260,7 @@ class TS_Helper():
         sess = T.compat.v1.Session(config = T.compat.v1.ConfigProto(gpu_options = gpu_options))
         return sess 
     
-    def plot_history(self,history):
+    def plot_history(self,history:dict) -> None:
         hist = pd.DataFrame(history.history)
         hist['epoch'] = history.epoch
 
@@ -254,7 +280,7 @@ class TS_Helper():
 
         plt.show()
 
-    def calc_confusion_matrix(self,prediction_df):
+    def calc_confusion_matrix(self,prediction_df) -> Tuple[np.ndarray,Tuple[int,int,int,int]]:
         """
         prediction_df: dataframe with predictions and target values
         """
@@ -267,7 +293,7 @@ class TS_Helper():
         FN = confusion[1, 0]
         return confusion, (TP, TN, FP, FN)
 
-    def set_prediction_threshold(self,model:T.keras.models.Sequential(),X_data:pd.DataFrame,y_data:pd.DataFrame,task:int,threshold:float):
+    def set_prediction_threshold(self,model:T.keras.models.Sequential(),X_data:pd.DataFrame,y_data:pd.DataFrame,task:int,threshold:float) -> pd.DataFrame:
         """
         model: trained model
         X_data: input data
@@ -283,7 +309,7 @@ class TS_Helper():
         #print(predictions_df)
         return predictions_df
 
-    def calc_Statistics(self,TP:int,TN:int,FP:int,FN:int,prediction_df):
+    def calc_Statistics(self,TP:int,TN:int,FP:int,FN:int,prediction_df) -> Tuple[str,tuple[float,float,float,float,float,float,float,float,float,float,float,float,float]]:
         """
         TP: True Positive
         TN: True Negative
@@ -306,7 +332,7 @@ class TS_Helper():
         statistics = tabulate.tabulate([["Accuracy",accuracy],["Precision",precision],["Recall",recall],["F1",f1],["MCC",MCC],["Kappa",kappa],["SE",SE],["SP",SP],["PPV",PPV],["NPV",NPV],["TPR",TPR],["FPR",FPR]],headers=["Statistic","Value"])
         return statistics,(accuracy, precision, recall, f1, MCC, kappa, SE, SP, PPV, NPV, TPR, FPR)
 
-    def calc_RegressionStatistics(self,prediction_df):
+    def calc_RegressionStatistics(self,prediction_df)-> Tuple[str,tuple[float,float,float]]:
         """
         prediction_df: dataframe with predictions and target values
         """
@@ -317,7 +343,7 @@ class TS_Helper():
         statistics = tabulate.tabulate([["MSE",MSE],["MAE",MAE],["R2",R2]],headers=["Statistic","Value"])
         return statistics,(MSE, MAE, R2)
 
-    def plot_Regression(self,X_train,X_val,X_test):
+    def plot_Regression(self,X_train,X_val,X_test)-> None:
         """
         prediction_df: dataframe with predictions and target values
         """
@@ -343,7 +369,7 @@ class TS_Helper():
         # Show the plot
         plt.show()
 
-    def plot_Classification(self,cm, classes, title='Confusion Matrix', cmap=plt.cm.Blues):
+    def plot_Classification(self,cm, classes, title='Confusion Matrix', cmap=plt.cm.Blues)-> None:
         """
         Plot a confusion matrix using Matplotlib.
         :param cm: Confusion matrix
@@ -378,7 +404,7 @@ class TS_Helper():
         fig.tight_layout()
         plt.show()
 
-    def get_RegressionStats(self,model:T.keras.models.Sequential(),X_train,y_train,X_test,y_test,X_val,y_val,task:int):
+    def get_RegressionStats(self,model:T.keras.models.Sequential(),X_train,y_train,X_test,y_test,X_val,y_val,task:int) -> Tuple[str,tuple[float,float,float]]:
         regression_prediction_train = pd.DataFrame(data={"pred":model.predict(X_train)[:,task],"y":y_train[:,task]},index=range(len(y_train)))
         regression_prediction_val = pd.DataFrame(data={"pred":model.predict(X_val)[:,task],"y":y_val[:,task]},index=range(len(y_val)))
         regression_prediction_test = pd.DataFrame(data={"pred":model.predict(X_test)[:,task],"y":y_test[:,task]},index=range(len(y_test)))
@@ -399,7 +425,7 @@ class TS_Helper():
 
         return train_text+"\n"+val_text+"\n"+test_text,train_val_test
     
-    def get_ClassificationStats(self,model,X_train:pd.DataFrame,y_train,X_test:pd.DataFrame,y_test,X_val:pd.DataFrame,y_val,task:int,threshold:float):
+    def get_ClassificationStats(self,model,X_train:pd.DataFrame,y_train,X_test:pd.DataFrame,y_test,X_val:pd.DataFrame,y_val,task:int,threshold:float) -> Tuple[str, tuple[float,float,float],Tuple[np.ndarray],Tuple[np.ndarray],Tuple[np.ndarray]]:
         classification_prediction_train = self.set_prediction_threshold(model, X_train, y_train, task, threshold)
         classification_prediction_val = self.set_prediction_threshold(model, X_val, y_val, task, threshold)
         classification_prediction_test = self.set_prediction_threshold(model, X_test, y_test, task, threshold)
@@ -424,7 +450,7 @@ class TS_Helper():
 
         return train_text+"\n"+val_text+"\n"+test_text,train_val_test,train_confusion_matrix,val_confusion_matrix,test_confusion_matrix
     
-    def get_modelStats(self,model:T.keras.models.Sequential(),X_train,y_train,X_test,y_test,X_val,y_val,tasks:int,threshold:float=0.5):
+    def get_modelStats(self,model:T.keras.models.Sequential(),X_train,y_train,X_test,y_test,X_val,y_val,tasks:int,threshold:float=0.5) -> None:
         if self.model_type == 0:
             print("Metric for a Classification Model")
         else:
@@ -441,4 +467,114 @@ class TS_Helper():
             if self.model_type == 1:
                 full_text,cross_val = self.get_RegressionStats(model,X_train,y_train,X_test,y_test,X_val,y_val,task)
                 print(full_text)
-                self.plot_Regression(cross_val[0],cross_val[1],cross_val[2])
+                self.plot_Regression(cross_val[0],cross_val[1],cross_val[2])              
+
+class Model_Generator(TS_Helper):
+    def __init__(self,):
+        super().__init__()
+    
+        Models = {
+        "Dense":T.keras.models.Sequential([
+            T.keras.layers.Dense(64,activation="relu"),
+            T.keras.layers.Dense(64,activation="relu"),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "LSTM":T.keras.models.Sequential([
+            T.keras.layers.LSTM(64,activation="relu",return_sequences=True),
+            T.keras.layers.LSTM(64,activation="relu"),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "GRU":T.keras.models.Sequential([
+            T.keras.layers.GRU(64,activation="relu",return_sequences=True),
+            T.keras.layers.GRU(64,activation="relu"),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "Conv1D":T.keras.models.Sequential([
+            T.keras.layers.Conv1D(64,3,activation="relu"),
+            T.keras.layers.MaxPool1D(3),
+            T.keras.layers.Conv1D(64,3,activation="relu"),
+            T.keras.layers.MaxPool1D(3),
+            T.keras.layers.Flatten(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "Conv2D":T.keras.models.Sequential([
+            T.keras.layers.Conv2D(64,3,activation="relu"),
+            T.keras.layers.MaxPool2D(3),
+            T.keras.layers.Conv2D(64,3,activation="relu"),
+            T.keras.layers.MaxPool2D(3),
+            T.keras.layers.Flatten(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "Conv3D":T.keras.models.Sequential([
+            T.keras.layers.Conv3D(64,3,activation="relu"),
+            T.keras.layers.MaxPool3D(3),
+            T.keras.layers.Conv3D(64,3,activation="relu"),
+            T.keras.layers.MaxPool3D(3),
+            T.keras.layers.Flatten(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "RNN":T.keras.models.Sequential([
+            T.keras.layers.SimpleRNN(64,activation="relu",return_sequences=True),
+            T.keras.layers.SimpleRNN(64,activation="relu"),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "Bidirectional":T.keras.models.Sequential([
+            T.keras.layers.Bidirectional(T.keras.layers.LSTM(64,activation="relu",return_sequences=True)),
+            T.keras.layers.Bidirectional(T.keras.layers.LSTM(64,activation="relu")),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "TimeDistributed":T.keras.models.Sequential([
+            T.keras.layers.TimeDistributed(T.keras.layers.Dense(64,activation="relu")),
+            T.keras.layers.TimeDistributed(T.keras.layers.Dense(64,activation="relu")),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "Attention":T.keras.models.Sequential([
+            T.keras.layers.Attention(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "Transformer":T.keras.models.Sequential([
+            T.keras.layers.Transformer(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "TransformerEncoder":T.keras.models.Sequential([
+            T.keras.layers.TransformerEncoder(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "TransformerDecoder":T.keras.models.Sequential([
+            T.keras.layers.TransformerDecoder(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "TransformerEncoderLayer":T.keras.models.Sequential([
+            T.keras.layers.TransformerEncoderLayer(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "TransformerDecoderLayer":T.keras.models.Sequential([
+            T.keras.layers.TransformerDecoderLayer(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "TransformerEncoderBlock":T.keras.models.Sequential([
+            T.keras.layers.TransformerEncoderBlock(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "TransformerDecoderBlock":T.keras.models.Sequential([
+            T.keras.layers.TransformerDecoderBlock(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "TransformerEncoderStack":T.keras.models.Sequential([
+            T.keras.layers.TransformerEncoderStack(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "TransformerDecoderStack":T.keras.models.Sequential([
+            T.keras.layers.TransformerDecoderStack(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "TransformerEncoderHead":T.keras.models.Sequential([
+            T.keras.layers.TransformerEncoderHead(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+        "TransformerDecoderHead":T.keras.models.Sequential([
+            T.keras.layers.TransformerDecoderHead(),
+            T.keras.layers.Dense(1,activation="sigmoid")
+        ]),
+    }
+
